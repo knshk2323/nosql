59e473dd2fbab83f9649da4fb0ec4d261c243009
knshk2323 <kunshuak.06@gmail.com>
2025-02-26 17:26:14 +0500

    Delete nosql_final directory

--- nosql_final/models/reader.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const readerSchema = new mongoose.Schema({
-  name: String,
-  email: String
-});
-
-module.exports = mongoose.model('Reader', readerSchema);
